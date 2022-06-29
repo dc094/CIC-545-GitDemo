b9ef346e90f05cb7a0329c3f763a096af6464fe9
dc094 <durgeshchavan4891@gmail.com>
2022-06-29 09:31:42 +0530

    Added: UC1 with user inputs

--- Sorting.sh
+++ Sorting.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 	
 echo "Welcome to shell scripting program: Arithmatic Computation & Sorting."
+
+#Take User Input
+
+read -p "Enter the value of a: " a;
+read -p "Enter the value of b: " b;
+read -p "Enter the value of c: " c;

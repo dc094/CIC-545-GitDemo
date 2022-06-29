01c5a81fd157aa4fec029beac64ac19caa7b71b2
dc094 <durgeshchavan4891@gmail.com>
2022-06-29 09:52:29 +0530

    Added: UC2 with Arithmatic Expression

--- Sorting.sh
+++ Sorting.sh
@@ -7,3 +7,7 @@ echo "Welcome to shell scripting program: Arithmatic Computation & Sorting."
 read -p "Enter the value of a: " a;
 read -p "Enter the value of b: " b;
 read -p "Enter the value of c: " c;
+
+expr[0]=$(($a+$b*c));
+
+echo ${expr[0]};
